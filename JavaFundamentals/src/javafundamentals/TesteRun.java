f759af237d3744da55bc087b87204a0bf449b1b0
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2023-08-17 06:13:33 -0300

    add-02: testing java execution.

---
+++ JavaFundamentals/src/javafundamentals/TesteRun.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafundamentals;
+
+/**
+ *
+ * @author maumneto
+ */
+public class TesteRun {
+    public static void main(String args[]) {
+        System.out.println("Testando a Execução!");
+    }
+}
