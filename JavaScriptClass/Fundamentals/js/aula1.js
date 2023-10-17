72cd6acedcec6cbd58122ceba8fddd6e08983376
Maurício Moreira Neto <mauricio.moreira@unichristus.edu.br>
2023-10-17 08:22:27 -0300

    add: javascript class

---
+++ JavaScriptClass/Fundamentals/js/aula1.js
@@ -0,0 +1,26 @@
+var a;
+console.log(a);
+var obj = null;
+console.log(obj);
+
+var b = 10;
+console.log( typeof b);
+var c = "Hello";
+console.log( typeof c);
+var d = {};
+console.log( typeof d);
+var e = [];
+console.log( typeof e);
+
+const pi = 3.1415;
+console.log(pi);
+
+{
+    let element = 10;
+    console.log(element);
+}
+
+for (var i = 0; i <= 10; i++) {
+    console.log(i);
+}
+console.log("value i: " + i);
---
+++ JavaScriptClass/Fundamentals/js/script.js
@@ -0,0 +1,1 @@
+window.alert("Estou sendo carregado por fora!");
